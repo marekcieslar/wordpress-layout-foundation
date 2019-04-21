6909e18313fa110be38cc0336ce48b46bc7e8b40
marekcieslar <marek.cieslar@gmail.com>
2019-04-21 12:26:09 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,4 +1,17 @@
 <footer>
-
-<p>from footer</p>
+  <div class="footer container" id="contact">
+    <h3>Fundacja Przystań Medyczna</h3>
+    <div class="footer__data">
+      <div class="footer__data__address">30-110 Kraków<br>ul. Józefa Ignacego Kraszewskiego 19/8<br>
+        REGON: 367370600<br>
+        NIP: 6772420946<br></div>
+      <div class="footer__data__phone">Mateusz Gajda: 792 901 908<br>Katarzyna Wanat: 665 588 105</div>
+      <div class="footer__data__mails"><a href="mailto:fundacja@przystanmedyczna.org" target="_blank"
+          rel="noopener">fundacja@przystanmedyczna.org</a><a href="mailto:rada@przystanmedyczna.org" target="_blank"
+          rel="noopener">rada@przystanmedyczna.org</a><a href="mailto:zarzad@przystanmedyczna.org" target="_blank"
+          rel="noopener">zarzad@przystanmedyczna.org</a></div>
+    </div>
+    <div class="footer__copy">© 2017 - <?php echo date('Y'); ?> <?php get_bloginfo('name') ?>, przygotowanie <a href="http://emfotografia.eu"
+        target="_blank">Marek Cieślar</a></div>
+  </div>
 </footer>
