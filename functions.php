48dd3bb462f2ead11273a081d3d2f454d5c3572b
marekcieslar <marek.cieslar@gmail.com>
2019-04-21 12:19:52 +0200

    - -> _

--- functions.php
+++ functions.php
@@ -2,21 +2,21 @@
 /**
  * Enqueue scripts and styles.
  */
-function mc-foundation_scripts() {
-	wp_enqueue_style( 'mc-foundation-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
+function mc_foundation_scripts() {
+	wp_enqueue_style( 'mc_foundation-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
 
-	wp_style_add_data( 'mc-foundation-style', 'rtl', 'replace' );
+	wp_style_add_data( 'mc_foundation-style', 'rtl', 'replace' );
 
 	if ( has_nav_menu( 'menu-1' ) ) {
-		wp_enqueue_script( 'mc-foundation-priority-menu', get_theme_file_uri( '/js/priority-menu.js' ), array(), '1.1', true );
-		wp_enqueue_script( 'mc-foundation-touch-navigation', get_theme_file_uri( '/js/touch-keyboard-navigation.js' ), array(), '1.1', true );
+		wp_enqueue_script( 'mc_foundation-priority-menu', get_theme_file_uri( '/js/priority-menu.js' ), array(), '1.1', true );
+		wp_enqueue_script( 'mc_foundation-touch-navigation', get_theme_file_uri( '/js/touch-keyboard-navigation.js' ), array(), '1.1', true );
 	}
 
-	wp_enqueue_style( 'mc-foundation-print-style', get_template_directory_uri() . '/print.css', array(), wp_get_theme()->get( 'Version' ), 'print' );
+	wp_enqueue_style( 'mc_foundation-print-style', get_template_directory_uri() . '/print.css', array(), wp_get_theme()->get( 'Version' ), 'print' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'mc-foundation_scripts' );
+add_action( 'wp_enqueue_scripts', 'mc_foundation_scripts' );
 ?>
