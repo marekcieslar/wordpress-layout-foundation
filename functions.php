ccc4b0176e327efb37fa38ae8d741eb974de6151
marekcieslar <marek.cieslar@gmail.com>
2019-04-21 17:37:46 +0200

    footer customize data

--- functions.php
+++ functions.php
@@ -16,3 +16,33 @@ function register_mc_foundation_menu() {
   register_nav_menu('header-menu',__( 'Menu Główne' ));
 }
 add_action( 'init', 'register_mc_foundation_menu' );
+
+function mc_foundation_customize_register( $wp_customize )
+{
+
+    //footer section
+    $wp_customize->add_section('mc_foundation_footer', array(
+        'title' => __('Footer', 'mc_foundation'),
+        'description' => 'editing footer data',
+        'priority' => 2
+    ));
+    $wp_customize->add_setting('mc_foundation_footer_title', array(
+        'default' => 'mc-foundation footer title'
+    ));
+    $wp_customize->add_control('mc_foundation_footer_title', array(
+        'label' => __('Footer title', 'mc_foundation'),
+        'section' => 'mc_foundation_footer',
+        'setting' => 'mc_foundation_footer_title'
+    ));
+    $wp_customize->add_setting('mc_foundation_footer_contact', array(
+        'default' => 'Contact<br>Data<br>is<br>here'
+    ));
+    $wp_customize->add_control('mc_foundation_footer_contact', array(
+        'label' => __('Header description', 'mc_foundation'),
+        'section' => 'mc_foundation_footer',
+        'setting' => 'mc_foundation_footer_contact',
+        'type' => 'textarea'
+		));
+	}
+
+	add_action( 'customize_register', 'mc_foundation_customize_register' );
